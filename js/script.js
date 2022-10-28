4622fc0e6f299ccbd7f79a2c865834138387f4ff
Nayane Souza <113534680+nayanesouza40@users.noreply.github.com>
2022-10-28 08:47:45 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,16 +1,11 @@
-var botaoListaEspera = document.querySelector('#botaoListaEspera');
-var fecharListaEspera = document.querySelector('#fecharListaEspera');
+
 var botaoLogin = document.querySelector('#iconLogin')
 var fecharLogin = document.querySelector('#fecharLogin')
-botaoListaEspera.addEventListener('click',function(){
-    $('#escuro').css('display','block')
-})
-fecharListaEspera.addEventListener('click',function(){
-    $('#escuro').css('display','none')
-})
+
 botaoLogin.addEventListener('click',function(){
     $('#login').css('display','block')
 })
 fecharLogin.addEventListener('click',function(){
     $('#login').css('display','none')
 })
+
