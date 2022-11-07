e9411f728e170a15a5e5a9b6ea11dcf82e17e091
Nayane Souza <113534680+nayanesouza40@users.noreply.github.com>
2022-11-07 08:09:50 -0300

    Add files via upload

---
+++ js/login-senha.js
@@ -0,0 +1,35 @@
+function validarSenha() {
+	let usuario = document.getElementById("usuario").value;
+    let password = document.getElementById("password").value;
+	
+	 // só permitirá o envio se os dados forem fornecidos
+    if ((usuario.indexOf("@")) == -1 ||
+      (usuario.indexOf(".")) == -1) {
+        alert('Digite um usuário válido ')
+      } else if ((usuario == "") ||
+      (usuario == null)){
+        alert('Digite um usuário')
+      } else if (password=='') {
+        alert('Digite sua senha')
+      } else {
+        alert('Login efetuado!')
+        location.reload()
+      }
+	}
+
+  function validarrecuperacao(){
+    const Email = document.querySelector("#Email");
+
+  if (Email.value === "") {
+    alert('Digite um Email')
+  }                          
+
+  /* Permiti somente informação com @ e "." e Bloquear @ seguido de "."  */ 
+  else if (Email.value.indexOf("@") == -1 || Email.value.indexOf(".") == -1 || (Email.value.indexOf(".") - Email.value.indexOf("@") == 1 )){
+    alert('Email Invalido');
+  }
+   else {
+    alert('Enviado com Sucesso');
+    location.reload()
+  }
+}
