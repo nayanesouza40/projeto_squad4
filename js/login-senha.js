587938a5a63dad7bee07afaeae3b0e8cf29b29a3
Nayane Souza <113534680+nayanesouza40@users.noreply.github.com>
2022-11-07 08:27:39 -0300

    Update login-senha.js

--- js/login-senha.js
+++ js/login-senha.js
@@ -13,7 +13,7 @@ function validarSenha() {
         alert('Digite sua senha')
       } else {
         alert('Login efetuado!')
-        location.reload('https://nayanesouza40.github.io/projeto_squad4/index.html')
+        window.location.reload()
       }
 	}
 
